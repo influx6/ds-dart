47d29691399e5448c6fbf87ca936fa513f09edd5
Alexander Adeniyin Ewetumo <trinoxf@gmail.com>
2014-01-26 22:37:43 +0100

    removed wrappers and fixes a few bugs with dart2js

--- test/graphs/packages/ds/core/dsabstract.dart
+++ test/graphs/packages/ds/core/dsabstract.dart
@@ -1,5 +1,34 @@
 part of ds.core;
 
+class Counter{
+  num _count = 0;
+  dynamic handler;
+  
+  Counter(this.handler);
+  
+  num get counter => _count;
+  
+  void tick(){
+    _count += 1;
+  }
+  
+  void untick(){
+    _count -= 1;
+  }
+  
+  void detonate(){
+    _count = 0;
+  }
+  
+  String toString(){
+    return "Counter: "+this.counter.toString();
+  }
+}
+  
+class Comparable{
+  bool compare(dynamic d);
+}
+
 class dsImpl {
 	
 	void addAll();
@@ -50,6 +79,8 @@ abstract class dsFilter{
 }
 
 abstract class dsAbstractNode<T> implements Comparable{
+	dsAbstractNode<T> left;
+	dsAbstractNode<T> right;
 	T data;
 	bool _mark = false;
 	
